1ead7f196d9a9bff7a61fe7bb220ad8f007be5d0
s14965 <s14965@pjwstk.edu.pl>
2021-06-03 23:07:47 +0200

    named queries part 2

--- src/main/java/pj/pjatk/szymaj/movie/MovieRepository.java
+++ src/main/java/pj/pjatk/szymaj/movie/MovieRepository.java
@@ -1,10 +1,16 @@
 package pj.pjatk.szymaj.movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Query("select m from Movie m where m.id = ?1")
     Movie findBy(Long id);
-    List<Movie> findByName(String name);
+    @Query("select m from Movie m where m.title = ?1")
+    List<Movie> findByTitle(String name);
+    @Query("select m from Movie m where m.category = ?1")
     List<Movie> findByCategory(Category category);
-    List<Movie> findByAvailability(boolean isavailable);
+    @Query("select m from Movie m where m.isavailable = ?1")
+    List<Movie> findByAvailable(boolean isavailable);
 }
